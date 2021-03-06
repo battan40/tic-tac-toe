c13b98b38230c7ced88e24ed843c8f237549ea84
Angela Battillo <angela.battillo@gmail.com>
2021-03-06 14:11:13 -0700

    Begin main file for DOM calling to game

---
+++ main.js
@@ -0,0 +1,11 @@
+var game = new Game();
+
+var gameBoardSquares = document.querySelector('#gameBoardGrid');
+
+gameBoardSquares.addEventListener('click', pickSquare);
+
+function pickSquare(event) {
+  var clickedSquare = event.target.id;
+  game.playGame(clickedSquare);
+  changePlayer();
+}
