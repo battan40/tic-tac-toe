ad7a1ff9032afc2a658fa72f4430393e68a42c3b
Angela Battillo <angela.battillo@gmail.com>
2021-03-07 13:49:54 -0700

    Create functionality of initial game play on the data model

--- game.js
+++ game.js
@@ -4,64 +4,70 @@ class Game {
     this.gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     this.playerOne = new Player('player1', '♥️');
     this.playerTwo = new Player('player2', '⭐');
-    this.turnTracker = this.playerOne.id
+    this.turnTracker = this.playerOne.name;
     this.win = false;
   }
 
   playGame(chosenSquare) {
   console.log("play game param", chosenSquare);
-    if (this.playerOne.icon !== this.gameBoard[chosenSquare.id] && this.playerTwo.icon !== this.gameBoard[chosenSquare.id ]){
+    if (this.playerOne.icon && this.playerTwo.icon !== this.gameBoard[chosenSquare]){
+      this.addIcon(chosenSquare);
     }
-    this.addIcon(chosenSquare);
-    }
+  }
 
   addIcon(squareToChange) {
-    console.log("addIcon", squareToChange.id)
-    if (this.turnTracker === this.playerOne.id) {
-      this.gameBoard.splice([squareToChange.id], 1, this.playerOne.icon);
-      this.playerOne.iconCounter++
-      console.log(this.gameBoard[0])
-      this.playerOne.squaresPlayedList.push(parseInt(squareToChange));
-      this.searchForWin(this.playerOne);
+    console.log("addIcon", squareToChange)
+    if (this.turnTracker === this.playerOne.name) {
+      this.gameBoard.splice(squareToChange, 1, this.playerOne.icon);
+      // this.playerOne.iconCounter++
+      console.log("position on board", this.gameBoard)
+      console.log("should display turn change", this.turnTracker)
+      // this.playerOne.squaresPlayedList.push(parseInt(squareToChange));
+      // this.searchForWin(this.playerOne);
       this.changePlayer();
-    } else if (this.turnTracker === this.playerTwo.id) {
-      this.gameBoard.splice([squareToChange.id], 1, this.playerTwo.icon);
-      this.playerTwo.iconCounter++
-      console.log(this.gameBoard[1])
-      this.playerTwo.squaresPlayedList.push(parseInt(squareToChange));
-      this.searchForWin(this.playerTwo);
+    } else {
+      this.gameBoard.splice(squareToChange, 1, this.playerTwo.icon);
+      // this.playerTwo.iconCounter++
+      console.log(this.gameBoard)
+      console.log(this.turnTracker)
+      this.changePlayer();
     }
-      this.changePlayer();
+      // this.playerTwo.squaresPlayedList.push(parseInt(squareToChange));
+      // this.searchForWin(this.playerTwo);
   }
+
+
 
   changePlayer() {
-    if (this.turnTracker === playerOne.id) {
-      this.turnTracker = playerTwo.id;
+    if (this.turnTracker === this.playerOne.name) {
+      this.turnTracker = this.playerTwo.name;
       console.log(this.turnTracker);
+    } else {
+      this.turnTracker = this.playerOne.name;
     }
-  }
-
-
-  searchForWin(personPlaying) {
-    console.log("searchForWin", personPlaying)
-    for (var i = 0; i < allPossibleWins.length; i++) {
-      if (this.squaresPlayedList.includes(allPossibleWins[i])) {
-        personPlaying.winCount++
-        this.win = true;
-        console.log(personPlaying.winCount)
-        personPlaying.saveWinsToStorage();
-      }
-    }
-      this.searchForATie();
-  }
-
-  searchForATie() {
-    if (!allPossibleWins && this.playerOne.iconCounter + this.playerTwo.iconCounter === 9) {
-      this.terminateGame()
-    }
-  }
-
-  terminateGame() {
-    var currentGame = new Game();
   }
 }
+//
+//   searchForWin(personPlaying) {
+//     console.log("Why can't I make a current player param that will feed me the data from the previous run methods?", personPlaying)
+//     for (var i = 0; i < this.allPossibleWins.length; i++) {
+//       if (this.squaresPlayedList.includes(allPossibleWins[i])) {
+//         personPlaying.winCount++
+//         this.win = true;
+//         console.log(personPlaying.winCount)
+//         personPlaying.saveWinsToStorage();
+//       }
+//     }
+//       this.searchForATie();
+//   }
+//
+//   searchForATie() {
+//     if (!allPossibleWins && this.playerOne.iconCounter + this.playerTwo.iconCounter === 9) {
+//       this.terminateGame()
+//     }
+//   }
+//
+//   terminateGame() {
+//     var currentGame = new Game();
+//   }
+// }
