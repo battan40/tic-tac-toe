610e1c43d838a6627e82f58ddabaf3c2e0a4d8bf
Angela Battillo <angela.battillo@gmail.com>
2021-03-09 15:04:15 -0700

    Change heart emoji in each file it presents

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ class Game {
   constructor() {
     this.allPossibleWins = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]];
     this.gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    this.playerOne = new Player('player1', '♥️');
+    this.playerOne = new Player('player1', '💙');
     this.playerTwo = new Player('player2', '⭐');
     this.turnTracker = this.playerOne;
     this.win = false;
