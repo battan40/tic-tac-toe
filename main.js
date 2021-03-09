c60a003008febea3f29ae05f76f8f2dcda3cabc1
Angela Battillo <angela.battillo@gmail.com>
2021-03-08 18:27:08 -0700

    Remove all commented out code

--- main.js
+++ main.js
@@ -7,17 +7,14 @@ var turnDisplay = document.querySelector('#turnDisplay')
 
 gameBoardSquares.addEventListener('click', startGame);
 window.addEventListener('load', pageLoads);
-//
-// function startDataModel(event) {
-//   var clickedSquare = event.target.id;
-//   currentGame.playGame(clickedSquare);
-// }
+
 function pageLoads() {
   showCurrentTurn();
   displayWinCount();
-  //you want to get the wins from storage (that means retrieve)
-  // currentGame.playerOne.saveWinsToStorage();
-  // currentGame.playerTwo.saveWinsToStorage();
+  currentGame.playerOne.retrieveWinsFromStorage();
+  currentGame.playerTwo.retrieveWinsFromStorage();
+  console.log(currentGame.playerOne);
+  console.log(currentGame.playerTwo);
 }
 
 function startGame(event) {
@@ -27,8 +24,6 @@ function startGame(event) {
     showCurrentTurn();
     declarationOfGameStatus();
   }
-
-  //When a player plays a turn i want the hearts and stars to alternate with the messages, until a win or a draw; if there is a draw I want the message at the top to display a new string, then reset the whole gameBoard; if there is a win i want the message at the top to change and the wins to save and the whole board to reset
 }
 
 function showIcon(clickedSquareParam) {
@@ -49,12 +44,8 @@ function showCurrentTurn() {
     turnDisplay.innerText = `${currentGame.playerTwo.name} it's your turn`
   }
 }
-//When the page loads, it should have player1 on the left, playaer 2 on the right and an initializing phrase that says whose turn it is
-//Once the firsts player clicks I want to have the event trigger the icons to populate and the turns to switch until theres a win, when theres a win or a tie i want the innerHTML to change to display that and the win to save to local storage but the whole page to be reset
+
 function declarationOfGameStatus() {
-  // if (currentGame.win) {
-  // turnDisplay.innerText = 'Looks like we have a tie!'
-  //setTimeOut(startGameOver, 2000)
     if (currentGame.win) {
     turnDisplay.innerText = `${currentGame.turnTracker.name} IS THE WINNER!`
     displayWinCount();
@@ -74,7 +65,6 @@ function resetGame() {
     currentGame.turnTracker.squaresPlayedList = [];
     currentGame.turnTracker.iconCounter = 0;
     currentGame.win = false;
-    //update headers on load
   }
 }
 
